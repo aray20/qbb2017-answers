e26afbb5d3fb1adbeaea2af79939a865e9669ac3
Amanda Ray <aray20@jhu.edu>
2017-08-29 18:05:48 -0400

    revised day2 homework 1 out

--- day2-homework/day2-homework-1.py
+++ day2-homework/day2-homework-1.py
@@ -5,12 +5,18 @@ import sys
 fh = open(sys.argv[1])
 
 string = "DROME"
+count = 0
 
 for line in fh:
     if string in line:
         fields = line.rstrip("\r\n").split()
         if len(fields) != 4:
             continue
+        count += 1
+        if count >= 100:
+            break
         gene_name = fields[3]
         gene_name2 = fields[2]
-        print gene_name, gene_name2
+       
+        print gene_name, gene_name2 
+        
