4b6aba8d5a327be794409e9ee55dad3cec831701
Amanda Ray <aray20@jhu.edu>
2017-09-15 12:38:22 -0400

    re-did realignment script

--- week1/realign.py
+++ week1/realign.py
@@ -1,17 +1,13 @@
-"""DISCLAIMER: THIS CODE IS NOT WORKING. IT IS PURELY FOR THE SAKE OF TEMPORARY SUBMISSION. 
-GIVES ERROR THAT NO MODULE NAMED FASTA EVEN THOUGH IT IS IMPORTED."""
-
-
 #!/usr/bin/env python
 
-import FASTAParser
+import fasta
 import sys
 import itertools
 
-nt = open(sys.argv[1]) #nucleotides --> 1000_homo.fa
-pt = open(sys.argv[2]) #proteins --> alignment_pt.fa
+nt = open(sys.argv[1]) #1000_homo.fa
+pt = open(sys.argv[2]) #alignment_pt.fa
 
-nt_pt = open("alignment_nuc.fa","w")
+nt_pt = open("alignment_nuc.fa", "w")
 
 for (nt_id, nt_seq), (pt_id, pt_seq) in itertools.izip(fasta.FASTAReader(nt), fasta.FASTAReader(pt)):
     nt_pt.write(nt_id + "\n")
@@ -21,5 +17,5 @@ for (nt_id, nt_seq), (pt_id, pt_seq) in itertools.izip(fasta.FASTAReader(nt), fa
         else:
             nt_pt.write(nt_seq[:3])
             nt_seq = nt_seq[3:]
-
+        
     nt_pt.write("\n")
