0ea79fddba2349b74a4e3bc8d681bb5586c67f0e
Amanda Ray <aray20@jhu.edu>
2017-12-21 17:08:21 -0500

    terminal commands update

--- week1/hw_scripts.py
+++ week1/hw_scripts.py
@@ -1,11 +1,11 @@
 """1. query.tsv --> 1000_homologs.fa"""
 
-awk '{gsub ("-","")}{print ">" $1 "\n" $2}' blast_alignment.tsv > 1000_homo.fa
+awk '{gsub ("-","")}{print ">" $1 "\n" $2}' blast_alignment.tsv > 1000_logs.fa
 
 """2. 1000_homologs.fa --> 1000_h_proteins.fa"""
 
-transeq 1000_homo.fa 1000_h_pt.fa
+transeq 1000_logs.fa 1000_homo_pt.fa
 
 """3. 1000_h_proteins.fa --> alignment_prot.fa"""
 
-mafft 1000_h_pt.fa > alignment_pt.fa
+mafft 1000_homo_pt.fa > alignpt.fa
